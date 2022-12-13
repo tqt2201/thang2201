c88a0b8a26320aaf5cb0edad674edf0b15b53f81
tqt2201 <118880765+tqt2201@users.noreply.github.com>
2022-12-13 20:35:17 +0700

    Add files via upload

---
+++ lab_10.sql
@@ -0,0 +1,69 @@
+﻿------36.	Tính tổng số lượng của từng sản phẩm bán ra trong tháng 10/2006.
+SELECT MASP, COUNT(DISTINCT MASP) AS TONGSO
+FROM SANPHAM
+WHERE MASP IN(SELECT MASP
+FROM CTHD C INNER JOIN HOADON H
+ON C.SOHD = H.SOHD
+WHERE MONTH(NGHD) = 10 AND YEAR(NGHD) = 2006)
+GROUP BY MASP
+------37.	Tính doanh thu bán hàng của từng tháng trong năm 2006.
+SELECT MONTH(NGHD) AS THANG, SUM(TRIGIA) AS DOANHTHU
+FROM HOADON
+WHERE YEAR(NGHD) = 2006
+GROUP BY MONTH(NGHD)
+------38.	Tìm hóa đơn có mua ít nhất 4 sản phẩm khác nhau.
+SELECT *
+FROM HOADON
+WHERE SOHD IN(SELECT SOHD
+FROM CTHD
+WHERE SL >= 4)
+------39.	Tìm hóa đơn có mua 3 sản phẩm do “Viet Nam” sản xuất (3 sản phẩm khác nhau).
+SELECT *
+FROM HOADON
+WHERE SOHD IN(SELECT SOHD
+FROM CTHD C INNER JOIN SANPHAM S
+ON C.MASP = S.MASP
+WHERE NUOCSX = 'VIET NAM' AND SL >= 3)
+------40.	Tìm khách hàng (MAKH, HOTEN) có số lần mua hàng nhiều nhất.
+SELECT MAKH, HOTEN
+FROM KHACHHANG
+WHERE MAKH = (SELECT TOP 1 MAKH
+FROM HOADON
+GROUP BY MAKH
+ORDER BY COUNT(DISTINCT SOHD) DESC)
+------41.	Tháng mấy trong năm 2006, doanh số bán hàng cao nhất ?
+SELECT TOP 1 MONTH(NGHD) AS THANG_DOANHSO_MAX
+FROM HOADON
+WHERE YEAR(NGHD) = 2006
+GROUP BY MONTH(NGHD)
+ORDER BY SUM(TRIGIA) DESC
+------42.	Tìm sản phẩm (MASP, TENSP) có tổng số lượng bán ra thấp nhất trong năm 2006.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE MASP = (SELECT TOP 1 MASP
+FROM CTHD
+GROUP BY MASP
+ORDER BY SUM(SL) DESC)
+------43.	*Mỗi nước sản xuất, tìm sản phẩm (MASP,TENSP) có giá bán cao nhất.
+
+SELECT NUOCSX, MAX(GIA) AS MAX
+FROM SANPHAM
+GROUP BY NUOCSX
+
+SELECT B.NUOCSX, MASP, TENSP
+FROM (SELECT NUOCSX, MAX(GIA) AS MAX
+FROM SANPHAM
+GROUP BY NUOCSX) AS B LEFT JOIN SANPHAM S 
+ON S.GIA = B.MAX 
+WHERE B.NUOCSX = S.NUOCSX
+--44.	Tìm nước sản xuất sản xuất ít nhất 3 sản phẩm có giá bán khác nhau.
+
+--45.	*Trong 10 khách hàng có doanh số cao nhất, tìm khách hàng có số lần mua hàng nhiều nhất.
+
+SELECT TOP 10 MAKH
+FROM KHACHHANG
+ORDER BY DOANHSO DESC
+
+SELECT MAKH, COUNT(SOHD)
+FROM HOADON
+GROUP BY MAKH
