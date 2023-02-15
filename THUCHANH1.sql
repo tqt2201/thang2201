4c8edf2a69368a2ef7da458974c5ceb852f2da20
tqt2201 <118880765+tqt2201@users.noreply.github.com>
2023-02-15 10:58:58 +0700

    Add files via upload

---
+++ THUCHANH1.sql
@@ -0,0 +1,116 @@
+﻿CREATE DATABASE BAITAPTUAN1
+GO
+USE BAITAPTUAN1
+Exec sp_addtype 'Mota', 'nvarchar(40)','not null';
+Exec sp_addtype 'IDKH', 'char(10)','not null';
+Exec sp_addtype 'DT', 'char(12)','not null';
+
+-- 2. Tạo table
+CREATE TABLE SanPham (
+	MaSP CHAR(6) NOT NULL,
+	TenSP VARCHAR(20),
+	NgayNhap Date,
+	DVT CHAR(10),
+	SoLuongTon INT,
+	DonGiaNhap money,
+)
+CREATE TABLE HoaDon (
+	MaHD CHAR(10) NOT NULL,
+	NgayLap Date,
+	NgayGiao Date,
+	MaKH IDKH,
+	DienGiai Mota,
+)
+CREATE TABLE KhachHang (
+	MaKH IDKH,
+	TenKH NVARCHAR(30),
+	DiaCHi NVARCHAR(40),
+	DienThoai DT,
+)
+CREATE TABLE ChiTietHD (
+	MaHD CHAR(10) NOT NULL,
+	MaSP CHAR(6) NOT NULL,
+	SoLuong INT
+)
+
+-- 3. Trong Table HoaDon, sửa cột DienGiai thành nvarchar(100).
+ALTER TABLE HoaDon
+	ALTER COLUMN DienGiai NVARCHAR(100)
+
+-- 4. Thêm vào bảng SanPham cột TyLeHoaHong float
+ALTER TABLE SanPham
+	ADD TyLeHoaHong float
+
+-- 5. Xóa cột NgayNhap trong bảng SanPham
+ALTER TABLE SanPham
+	DROP COLUMN NgayNhap
+
+-- 6. Tạo các ràng buộc khóa chính và khóa ngoại cho các bảng trên
+ALTER TABLE SanPham
+ADD
+CONSTRAINT pk_sp primary key(MASP)
+
+ALTER TABLE HoaDon
+ADD
+CONSTRAINT pk_hd primary key(MaHD)
+
+ALTER TABLE KhachHang
+ADD
+CONSTRAINT pk_khanghang primary key(MaKH)
+
+ALTER TABLE HoaDon
+ADD
+CONSTRAINT fk_khachhang_hoadon FOREIGN KEY(MaKH) REFERENCES KhachHang(MaKH)
+
+ALTER TABLE ChiTietHD
+ADD
+CONSTRAINT fk_hoadon_chitiethd FOREIGN KEY(MaHD) REFERENCES HoaDon(MaHD)
+
+ALTER TABLE ChiTietHD
+ADD
+CONSTRAINT fk_sanpham_chitiethd FOREIGN KEY(MaSP) REFERENCES SanPham(MaSP)
+
+-- 7.Thêm vào bảng HoaDon các ràng buộc
+ALTER TABLE HoaDon
+ADD CHECK (NgayGiao > NgayLap)
+
+ALTER TABLE HoaDon
+ADD CHECK (MaHD like '[A-Z][A-Z][0-9][0-9][0-9][0-9]')
+
+ALTER TABLE HoaDon
+ADD CONSTRAINT df_ngaylap DEFAULT GETDATE() FOR NgayLap
+
+-- 8.Thêm vào bảng Sản phẩm các ràng buộc
+ALTER TABLE SanPham
+ADD CHECK (SoLuongTon > 0 and SoLuongTon < 50)
+
+ALTER TABLE SanPham
+ADD CHECK (DonGiaNhap > 0)
+
+ALTER TABLE SanPham
+ADD CONSTRAINT df_ngaynhap DEFAULT GETDATE() FOR NgayNhap
+
+ALTER TABLE SanPham
+ADD CHECK (DVT like 'KG''Thùng''Hộp''Cái')
+
+-- 9.Dùng lệnh T-SQL nhập dữ liệu vào 4 table trên, dữ liệu tùy ý, chú ý các ràng buộc của mỗi Table
+INSERT INTO SanPham (MaSP, TenSp, NgayNhap, DVT, SoLuongTon, DonGiaNhap) values ('001', 'maý tính', '2008-11-11', 'VND', 23, '2000000')
+INSERT INTO SanPham values ()
+INSERT INTO SanPham values ()
+INSERT INTO SanPham values ()
+
+INSERT INTO HoaDon (MaHD, NgayLap, NgayGiao, MaKH, DienGiai) values ('H01', )
+INSERT INTO HoaDon values ()
+INSERT INTO HoaDon values ()
+INSERT INTO HoaDon values ()
+
+INSERT INTO KhachHang (MaKH, TenKH, DiaCHi, DienThoai) values ()
+INSERT INTO KhachHang values ()
+INSERT INTO KhachHang values ()
+INSERT INTO KhachHang values ()
+
+INSERT INTO ChiTietHD (MaHD, MaSP, SoLuong) values ()
+INSERT INTO ChiTietHD values ()
+INSERT INTO ChiTietHD values ()
+INSERT INTO ChiTietHD values ()
+
---
+++ THUCHANH3.sql
@@ -0,0 +1,92 @@
+﻿CREATE DATABASE  MarkManagement;
+
+CREATE TABLE Students (
+StudentID NVARCHAR(12) PRIMARY KEY,
+StudentName NVARCHAR(25) not null,
+DateofBirth Datetime not null,
+Email NVARCHAR(40),
+Phone NVARCHAR(12),
+Class NVARCHAR(10),
+)
+CREATE TABLE Subjects (
+SubjectID NVARCHAR(10) PRIMARY KEY,
+SubjectName NVARCHAR(25) not null,
+)
+CREATE TABLE Mark (
+StudentID NVARCHAR(12),
+SubjectID NVARCHAR(10),
+Date Datetime not null,
+theory Tinyint,
+Practical Tinyint,
+PRIMARY KEY (StudentID, SubjectID)
+);
+
+INSERT INTO Students 
+VALUES ('AV0807005', 'Mail Trung Hiếu', '11/10/1989', 'trunghieu@yahoo.com', '0904115116', 'AV1'),
+('AV0807006', 'Nguyễn Quý Hùng', '2/12/1988', 'quyhung@yahoo.com', '0955667787', 'AV2'),
+('AV0807007', 'Đỗ Đắc Huỳnh', '2/1/1990', 'dachuynh@yahoo.com', '0988574747', 'AV2'),
+('AV0807009', 'An Đăng Khuê', '6/3/1986', 'dangkhue@yahoo.com', '0986757463', 'AV1'),
+('AV0807010', 'Nguyễn T. Tuyết Lan', '12/7/1989', 'tuyetlan@gmail.com', '0983310342', 'AV2'),
+('AV0807011', 'Đinh Phụng Long', '2/12/1990', 'phunglong@yahoo.com', NULL, 'AV1'),
+('AV0807012', 'Nguyễn Tuấn Nam', '2/3/1990', 'tuannam@yahoo.com', NULL, 'AV1');
+
+INSERT INTO Subjects 
+VALUES ('S001', 'SQL'),
+('S002', 'Java Simplefield'),
+('S003', 'Active Server Page');
+
+INSERT INTO Mark 
+VALUES ('AV0807005', 'S001', '6/5/2008', 8, 25),
+('AV0807006', 'S002', '6/5/2008', 16, 30),
+('AV0807007', 'S001',  '6/5/2008',10, 25),
+('AV0807009', 'S003', '6/5/2008', 7, 13),
+('AV0807010', 'S003', '6/5/2008', 9, 16),
+('AV0807011', 'S002', '6/5/2008', 8, 30),
+('AV0807012', 'S001', '6/5/2008', 7, 31),
+('AV0807005', 'S002', '6/6/2008', 12, 11),
+('AV0807009', 'S003', '6/6/2008', 11, 20),
+('AV0807010', 'S001', '6/6/2008', 7, 6);
+
+--1. Hiển thị nội dung bảng Students
+SELECT * FROM Students
+GO
+
+--2. Hiển thị nội dung danh sách sinh viên lớp AV1
+SELECT * FROM Students
+WHERE Class = 'AV1';
+GO
+
+--3. Sử dụng lệnh UPDATE để chuyển sinh viên có mã AV0807012 sang lớp AV2
+UPDATE Students set Class = 'AV2' where StudentID = 'AV0807012'
+
+--4
+create proc tinhtongsv @malop nvarchar(10)
+as
+begin
+select count(StudentID) as 'Số lượng sinh viên',Class as 'mã lớp'
+from Students where Class = @malop
+group by Class
+end
+go
+exec tinhtongsv 'AV1'
+
+-- 5. Hiển thị danh sách sinh viên lớp AV2 được sắp xếp tăng dần theo StudentName
+Select * From Students Where Class='AV2' Order by StudentName
+
+-- 6. Hiển thị danh sách sinh viên không đạt lý thuyết môn S001 (theory <10) thi ngày 6/5/2008
+select * from Students inner join Mark ON Students.StudentID = Mark.StudentID
+Where SubjectID = 'S001' and theory < 10 and Date = '6/5/2008'
+
+-- 7. Hiển thị tổng số sinh viên không đạt lý thuyết môn S001. (theory <10)
+select count(Class) As 'Tổng số sinh viên' From Students inner join Mark ON Students.StudentID = Mark.StudentID
+Where SubjectID = 'S001' and theory < 10
+
+-- 8.Hiển thị Danh sách sinh viên học lớp AV1 và sinh sau ngày 1/1/1980
+select * from Students Where Class = 'AV1' and DateofBirth > '1/1/1980'
+
+--9. Xoá sinh viên có mã AV0807011
+DELETE Students FROM Students
+WHERE StudentID = 'AV0807011'
+
+-- 10. Hiển thị danh sách sinh viên dự thi môn có mã S001 ngày 6/5/2008 bao gồm các trường sau: StudentID, StudentName, SubjectName, Theory, Practical, Date
+select Students.StudentID,Mark.SubjectID,Theory,Practical,Date from Students inner join Mark ON Students.StudentID = Mark.StudentID where SubjectID = 'S001' and Date = 6/5/2008
